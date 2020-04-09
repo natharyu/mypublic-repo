cc7d87df4c02775ddef77a6bcdb4826f63355cfa
Natharyu <anthony.dewittelebrun@gmail.com>
2020-04-09 16:36:30 +0200

    Rendu final exercice Ruby 2/2

---
+++ Ruby-datascientist/README.md
@@ -0,0 +1,9 @@
+# Rendu exercice Ruby 2/2
+
+
+Bonjour,
+Voici le rendu des exercices
+- 00_journalists.rb
+- 01_cryptocurrencies.rb
+
+Tout est dispo sur le repo. Bonne correction ;)
---
+++ Ruby-datascientist/lib/00_journalists.rb
@@ -0,0 +1,128 @@
+$handle = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+
+# Affiche la taille du tableau
+def tailleTableau(tableau)
+    return tableau.size
+end
+
+# Retourne le nombre d'handle avec un numero
+def handleWithNumeric(tableau)
+    countNumero = 0
+
+    tableau.each do |element|
+        # puts element
+        if element =~ /[0-9]/
+            countNumero = countNumero + 1
+        end
+    end
+
+    return countNumero
+end
+
+# Retourne le nombre d'handle avec 'aude'
+def handleWithAude(tableau)
+    countNumero = 0
+
+    tableau.each do |element|
+        # puts element
+        if element =~ /[aA][uU][dD][eE]/
+        # if element =~ /aude/i #i for ignore case / case insensitive 
+            countNumero = countNumero + 1
+        end
+    end
+
+    return countNumero
+end
+
+# Retourne le nombre d'handle avec une majuscule apres le @
+def handleWithMajAfter(tableau)
+    countNumero = 0
+
+    tableau.each do |element|
+        # puts element
+        if element =~ /@[A-Z]/
+            countNumero = countNumero + 1
+        end
+    end
+
+    return countNumero
+end
+
+# Retourne le nombre d'handle avec au moins une majuscule 
+def handleWithAMaj(tableau)
+    countNumero = 0
+
+    tableau.each do |element|
+        # puts element
+        if element =~ /[A-Z]/
+            countNumero = countNumero + 1
+        end
+    end
+
+    return countNumero
+end
+
+# Retourne le nombre total de underscore 
+def handleWithUnderScore(tableau)
+    countNumero = 0
+
+    tableau.each do |element|
+        # puts element
+        if element =~ /_/
+            countNumero = countNumero + 1
+        end
+    end
+
+    return countNumero
+end
+
+# Retourne le tableau dans l'ordre alphabetique 
+def alphaSort(tableau)
+    # tableauSorted = tableau.sort
+    # Sort alphabétique case insensitive: comparaison des version downcase (sans majuscule)
+    tableauSorted = tableau.sort do |a, b|
+        a.downcase <=> b.downcase
+    end
+
+    return tableauSorted
+end
+
+def alphaLenght(tableau)
+    # tableauSorted = tableau.sort
+    tableauSorted = tableau.sort_by(&:length)    
+    return tableauSorted
+end
+
+# The main function
+def main()
+    totalJournalistes = tailleTableau($handle)
+    puts "Il y a #{totalJournalistes} journalistes"
+
+    totalNumeric = handleWithNumeric($handle)
+    puts "il y a #{totalNumeric} qui ont des numéros !"
+
+    totalAude = handleWithAude($handle)
+    puts "il y a #{totalAude} qui ont Aude dans leur nom !"
+
+    totalMajAfter = handleWithMajAfter($handle)
+    puts "il y a #{totalMajAfter} qui ont une majuscule après le '@' !"
+
+    totalAMaj = handleWithAMaj($handle)
+    puts "il y a #{totalAMaj} qui ont au moins une majuscule"
+
+    totalUnderScore = handleWithUnderScore($handle)
+    puts "il y a #{totalUnderScore} '_' parmis la totalité des pseudos"
+    puts
+
+    sortAlpha = alphaSort($handle)
+    puts "Liste sort alphabétique: #{sortAlpha.join(', ')}"
+    puts
+
+    sortLength = alphaLenght($handle)
+    puts "Liste des 50 premiers handle les plus courts: #{sortLength[0..49].join(', ')}"
+    puts
+    
+    puts "Index de @epenser: #{$handle.index("@epenser")}"
+end
+
+main()
